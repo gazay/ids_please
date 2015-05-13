97d0372e0c8368fda162d34b89a77478bae73e04
Alexey Gaziev <alex.gaziev@gmail.com>
2015-05-13 12:03:31 +0800

    Facebook grabber

--- lib/ids_please/grabbers/base.rb
+++ lib/ids_please/grabbers/base.rb
@@ -13,6 +13,21 @@ class IdsPlease
       def grab
       end
 
+      def to_s
+        line = ''
+        self.instance_variables.each do |iv|
+          next if iv == :@page_source
+          val = self.instance_variable_get(iv)
+          next if val.nil? || val == ''
+          line += ", #{iv}=#{val}"
+        end
+        "#{self.class}##{self.object_id} #{line[1..-1]}"
+      end
+
+      def inspect
+        to_s
+      end
+
     end
   end
 end
--- lib/ids_please/grabbers/facebook.rb
+++ lib/ids_please/grabbers/facebook.rb
@@ -6,10 +6,12 @@ class IdsPlease
         @page_source = open(link).read
         @network_id  = @page_source.scan(/entity_id":"(\d+)"/).flatten.first
         @avatar = @page_source.scan(/og:image" content="([^"]+)"/).flatten.first
+        @avatar = CGI.unescapeHTML(@avatar)
         @display_name = @page_source.scan(/og:title" content="([^"]+)"/).flatten.first
+        @display_name = CGI.unescapeHTML(@display_name)
         @data = {
-          type: @page_source.scan(/og:type" content="([^"]+)"/).flatten.first,
-          description: @page_source.scan(/og:description" content="([^"]+)"/).flatten.first
+          type: CGI.unescapeHTML(@page_source.scan(/og:type" content="([^"]+)"/).flatten.first),
+          description: CGI.unescapeHTML(@page_source.scan(/og:description" content="([^"]+)"/).flatten.first)
         }
       end
 
