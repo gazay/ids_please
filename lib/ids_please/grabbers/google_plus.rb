96d9138e3434b2f52f2efef34003513c8a319d60
Michael Bastos <bastosmichael@gmail.com>
2015-08-14 13:31:10 -0700

    Finished making changes

--- lib/ids_please/grabbers/google_plus.rb
+++ lib/ids_please/grabbers/google_plus.rb
@@ -3,10 +3,10 @@ class IdsPlease
     class GooglePlus < IdsPlease::Grabbers::Base
 
       def grab_link
-        @network_id  = page_source.scan(/data-oid="(\d+)"/).flatten.first
-        @avatar = 'https:' + page_source.scan(/guidedhelpid="profile_photo"><img src="([^"]+)"/).flatten.first
+        @network_id   = page_source.scan(/data-oid="(\d+)"/).flatten.first
+        @avatar       = 'https:' + page_source.scan(/guidedhelpid="profile_photo"><img src="([^"]+)"/).flatten.first
         @display_name = page_source.scan(/og:title" content="([^"]+)"/).flatten.first.gsub(' - Google+','')
-        @username = '+' + page_source.scan(/&quot;https:\/\/plus.google.com\/\+(.+?)&quot;/).flatten.first
+        @username     = '+' + page_source.scan(/&quot;https:\/\/plus.google.com\/\+(.+?)&quot;/).flatten.first
         @data = {
           description: page_source.scan(/name="Description" content="([^"]+)">/).flatten.first.encode('utf-8')
         }
