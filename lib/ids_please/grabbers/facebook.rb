d1fb8fc984688ec862b8cc84ab1f8bd73e46b293
Michael Bastos <bastosmichael@gmail.com>
2015-08-17 10:35:42 -0700

    refactored facebook

--- lib/ids_please/grabbers/facebook.rb
+++ lib/ids_please/grabbers/facebook.rb
@@ -3,26 +3,20 @@ class IdsPlease
     class Facebook < IdsPlease::Grabbers::Base
 
       def grab_link
-        @network_id   = page_source.scan(/entity_id":"(\d+)"/).flatten.first
-        @avatar       = page_source.scan(/og:image" content="([^"]+)"/).flatten.first
-        @display_name = page_source.scan(/og:title" content="([^"]+)"/).flatten.first
-        @username     = page_source.scan(/og:url" content="[^"]+\/([^\/"]+)"/).flatten.first
-        @avatar       = CGI.unescapeHTML(@avatar.encode('utf-8')) if @avatar
-        @display_name = CGI.unescapeHTML(@display_name.encode('utf-8')) if @display_name
+        @network_id   = find_network_id
+        @avatar       = find_avatar
+        @display_name = find_display_name
+        @username     = find_username
 
         @counts = {
-          likes:  likes,
-          visits: visits,
+          likes:  find_likes,
+          visits: find_visits,
         }.delete_if {|k,v| v.nil? }
 
-        @data = {}
-        {
-          type: page_source.scan(/og:type" content="([^"]+)"/).flatten.first.encode('utf-8'),
-          description: page_source.scan(/og:description" content="([^"]+)"/).flatten.first.encode('utf-8'),
-        }.each do |k, v|
-          next if v.nil? || v == ''
-          @data[k] = CGI.unescapeHTML(v).strip
-        end
+        @data = {
+          type: find_type,
+          description: find_description
+        }.delete_if {|k,v| v.nil? }
 
         self
       rescue => e
@@ -31,56 +25,60 @@ class IdsPlease
       end
 
       def find_network_id
-
+        page_source.scan(/entity_id":"(\d+)"/).flatten.first
       rescue => e
         record_error __method__, e.message
         return nil
       end
 
       def find_avatar
-
+        CGI.unescapeHTML(page_source.scan(/og:image" content="([^"]+)"/).flatten.first.encode('utf-8'))
       rescue => e
         record_error __method__, e.message
         return nil
       end
 
       def find_display_name
-
+        CGI.unescapeHTML(page_source.scan(/og:title" content="([^"]+)"/).flatten.first.encode('utf-8'))
       rescue => e
         record_error __method__, e.message
         return nil
       end
 
       def find_username
-
+        page_source.scan(/og:url" content="[^"]+\/([^\/"]+)"/).flatten.first
       rescue => e
         record_error __method__, e.message
         return nil
       end
 
       def find_type
-
+        CGI.unescapeHTML(page_source.scan(/og:type" content="([^"]+)"/).flatten.first.encode('utf-8')).strip
       rescue => e
         record_error __method__, e.message
         return nil
       end
 
       def find_description
-
+        CGI.unescapeHTML(page_source.scan(/og:description" content="([^"]+)"/).flatten.first.encode('utf-8')).strip
       rescue => e
         record_error __method__, e.message
         return nil
       end
 
       def find_likes
-        page_source.scan(/>([^"]+) <span class=".+">likes/).flatten.first.tr(',','').to_i
+        if likes = page_source.scan(/>([^"]+) <span class=".+">likes/).flatten.first
+          likes.tr(',','').to_i
+        end
       rescue => e
         record_error __method__, e.message
         return nil
       end
 
       def find_visits
-        page_source.scan(/likes.+>([^"]+)<\/span> <span class=".+">visits/).flatten.first.tr(',','').to_i
+        if visits = page_source.scan(/likes.+>([^"]+)<\/span> <span class=".+">visits/).flatten.first
+          visits.tr(',','').to_i
+        end
       rescue => e
         record_error __method__, e.message
         return nil
