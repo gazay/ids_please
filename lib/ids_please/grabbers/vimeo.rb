c970d927a874aba250f5a54b9e3868b47f7c45ef
Jezreel <jezreel.go@gmail.com>
2015-10-14 16:36:26 -0700

    Add vimeo grabber with find avatar and display name

---
+++ lib/ids_please/grabbers/vimeo.rb
@@ -0,0 +1,32 @@
+class IdsPlease
+  module Grabbers
+    class Vimeo < IdsPlease::Grabbers::Base
+
+      def grab_link
+        @network_id   = find_network_id
+        @avatar       = find_avatar
+        @display_name = find_display_name
+        @username     = find_username
+
+        self
+      rescue => e
+        record_error __method__, e.message
+        return self
+      end
+
+      def find_avatar
+        find_by_regex(/og:image" content="([^"]+)"/)
+      rescue => e
+        record_error __method__, e.message
+        return nil
+      end
+
+      def find_display_name
+        find_by_regex(/og:title" content"([^"]+)"/)
+      rescue => e
+        record_error __method__, e.message
+        return nil
+      end
+    end
+  end
+end
